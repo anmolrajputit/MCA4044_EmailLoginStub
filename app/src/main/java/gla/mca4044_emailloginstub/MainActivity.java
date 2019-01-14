60f172ee540079a786de0565b62a4fbd97556bda
dbc2201 <divyanshb22@gmail.com>
2019-01-14 15:21:10 +0530

    add TODOs

--- app/src/main/java/gla/mca4044_emailloginstub/MainActivity.java
+++ app/src/main/java/gla/mca4044_emailloginstub/MainActivity.java
@@ -2,14 +2,52 @@ package gla.mca4044_emailloginstub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    //TODO #1 add object for EditTexts - et_email, et_password
+    // names will be etEmail & etPassword
+//    ex - EditText etEmail;
+    //TODO #2 add object for Button - btn_login
+    // name will be btnLogin
+//    Button btnLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO #3 match Java object etEmail to layout resource et_email
+//        etEmail = findViewById(R.id.et_email);
+        //TODO #4 match Java object etPassword to layout resource et_password
+
+        //TODO #5 match Java object btnLogin to layout resource btn_login
+//        btnLogin = findViewById(R.id.btn_login);
+
+        //TODO #6 add onCLickListener() method for 'btnlogin'
+//        btnLogin.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View v)
+//            {
+//                TODO #7 fetch the text of the EditText etEmail inside a string variable
+//                  String email = etEmail.getText().toString();
+//
+//                TODO #8 fetch the text of the EditText etPassword inside a string variable
+//                  String password = etPassword.getText().toString();
+//
+//                TODO #9 add Toast for displaying the values
+//                  Toast.makeText(this, email + ", " + password
+//                      , Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
     }
 }
