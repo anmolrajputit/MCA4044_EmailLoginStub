652da883d9085f05d1f7f6e32387c41ce10c2ef8
anmolrajputit <anmol.singh_mca18@gla.ac.in>
2019-01-14 18:13:47 +0530

    Used Toast

--- app/src/main/java/gla/mca4044_emailloginstub/MainActivity.java
+++ app/src/main/java/gla/mca4044_emailloginstub/MainActivity.java
@@ -13,6 +13,8 @@ public class MainActivity extends AppCompatActivity
     //TODO #1 add object for EditTexts - et_email, et_password
     // names will be etEmail & etPassword
 //    ex - EditText etEmail;
+    EditText etEmail,etPassword;
+    Button btnLogin;
     //TODO #2 add object for Button - btn_login
     // name will be btnLogin
 //    Button btnLogin;
@@ -25,29 +27,31 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         //TODO #3 match Java object etEmail to layout resource et_email
-//        etEmail = findViewById(R.id.et_email);
+        etEmail = findViewById(R.id.et_email);
         //TODO #4 match Java object etPassword to layout resource et_password
-
+        etPassword = findViewById(R.id.et_password);
         //TODO #5 match Java object btnLogin to layout resource btn_login
-//        btnLogin = findViewById(R.id.btn_login);
+        btnLogin = findViewById(R.id.btn_login);
 
         //TODO #6 add onCLickListener() method for 'btnlogin'
-//        btnLogin.setOnClickListener(new View.OnClickListener()
-//        {
-//            @Override
-//            public void onClick(View v)
-//            {
+        btnLogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
 //                TODO #7 fetch the text of the EditText etEmail inside a string variable
-//                  String email = etEmail.getText().toString();
-//
+                  String email = etEmail.getText().toString();
+
 //                TODO #8 fetch the text of the EditText etPassword inside a string variable
-//                  String password = etPassword.getText().toString();
-//
+                  String password = etPassword.getText().toString();
+
 //                TODO #9 add Toast for displaying the values
-//                  Toast.makeText(this, email + ", " + password
-//                      , Toast.LENGTH_SHORT).show();
-//            }
-//        });
+                 // Toast.makeText(this, etEmail + ", " + etPassword)
+                   //       Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),email+","+password,Toast.LENGTH_SHORT).show();
+            }
+
+      });
 //        TODO # 10 Run the app after writing the code and confirm it.
 
 //        contact : divyansh.bhardwaj@gla.ac.in for any queries
